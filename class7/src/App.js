f7a5e769151dd81e8542cdfbed5cbc559f9fdeb9
Faisal Ahmed <aaravkhan469@gmail.com>
2023-11-17 13:24:39 +0530

    React

--- class7/src/App.js
+++ class7/src/App.js
@@ -1,74 +1,101 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [click, setstate] = useState("")
+  const handleClick = (e) => {
+    // alert(e.target.value)
+    // alert(typeof e.target.value)
+    setstate(click.concat(e.target.value));
+  }
+
+  const clear = () => {
+    setstate("")
+  };
+
+  // const delete = () => {
+  //   setstate(click.slice(0, 3))
+  // }
+  const del = () => {
+    setstate(click.slice(0, -1))
+  }
+
+  const cal = () => {
+    try {
+      // eslint-disable-next-line no-eval
+      setstate(eval(click).toString())
+    } catch {
+      setstate("Error")
+    }
+  }
   return (
     <section>
         <div>
           <h1>CALCULATOR-PROJECT</h1>
-          <input type="text" />
+          <input type="text" value={click}/>
           <table>
             <tr>
               <td colSpan={2}>
-                <button>AC</button>
+                <button onClick={clear}>AC</button>
               </td>
               <td>
-                <button>DEL</button>
+                <button onClick={del}>DEL</button>
               </td>
               <td>
-                <button>&divide;</button>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <button>7</button>
-              </td>
-              <td>
-                <button>8</button>
-              </td>
-              <td>
-                <button>9</button>
-              </td>
-              <td>
-                <button>&times;</button>
+                <button value={"/"} onClick={handleClick}>&divide;</button>
               </td>
             </tr>
             <tr>
               <td>
-                <button>4</button>
+                <button value={"7"} onClick={handleClick}>7</button>
               </td>
               <td>
-                <button>5</button>
+                <button value={"8"} onClick={handleClick}>8</button>
               </td>
               <td>
-                <button>6</button>
+                <button value={"9"} onClick={handleClick}>9</button>
               </td>
               <td>
-                <button>&minus;</button>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <button>1</button>
-              </td>
-              <td>
-                <button>2</button>
-              </td>
-              <td>
-                <button>3</button>
-              </td>
-              <td>
-                <button>+</button>
+                <button  value={"*"} onClick={handleClick}>&times;</button>
               </td>
             </tr>
             <tr>
               <td>
-                <button>0</button>
+                <button value={"4"} onClick={handleClick}>4</button>
               </td>
               <td>
-                <button>.</button>
+                <button value={"5"} onClick={handleClick}>5</button>
+              </td>
+              <td>
+                <button value={"6"} onClick={handleClick}>6</button>
+              </td>
+              <td>
+                <button value={"-"} onClick={handleClick}>&minus;</button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button value={"1"} onClick={handleClick}>1</button>
+              </td>
+              <td>
+                <button value={"2"} onClick={handleClick}>2</button>
+              </td>
+              <td>
+                <button value={"3"} onClick={handleClick}>3</button>
+              </td>
+              <td>
+                <button value={"+"} onClick={handleClick}>+</button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button value={"0"} onClick={handleClick}>0</button>
+              </td>
+              <td>
+                <button value={"."} onClick={handleClick}>.</button>
               </td>
               <td colSpan={2}>
-                <button>=</button>
+                <button onClick={cal}>=</button>
               </td>
             </tr>
           </table>
